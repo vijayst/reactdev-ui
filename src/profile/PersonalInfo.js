74802311012f3ff1480aa893c7238978d3d291e3
vijayst <vijayst@gmail.com>
2018-07-06 19:09:34 +0530

    show profile pic

--- src/profile/PersonalInfo.js
+++ src/profile/PersonalInfo.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
+import Photo from '../images/vijay.jpg';
 
 export default class PersonalInfo extends Component {
     render() {
         return (
             <div className="personal-info-container">
+                <div className="photo">
+                    <img src={Photo} alt="" />
+                </div>
                 <div>First name:</div>
                 <div>Vijay</div>
                 <div>Last name:</div>
                 <div>Thirugnanam</div>
                 <div>Email:</div>
                 <div>vijayst@gmail.com</div>
-                <div>Photo:</div>
-                <div></div>
                 <div>City:</div>
                 <div>Bangalore</div>
                 <div>Country:</div>
