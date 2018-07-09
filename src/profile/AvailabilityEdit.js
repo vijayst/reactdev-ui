126fbf04b09f35e4b7863e147690da64e3fc32ff
Vijay Thirugnanam <vijayst@Vijays-MacBook-Air.local>
2018-07-09 19:32:14 +0530

    handler for switch

--- src/profile/AvailabilityEdit.js
+++ src/profile/AvailabilityEdit.js
@@ -2,12 +2,23 @@ import React, { Component } from 'react';
 import { Input, InputNumber, Select, Switch, DatePicker } from 'antd';
 
 export default class AvailabilityEdit extends Component {
+    state = {
+        available: false
+    };
+
+    handleAvailableChange(available) {
+        this.setState({ available });
+    }
+
     render() {
+        const { available } = this.state;
         return (
             <div className="availability-edit-container">
                 <div className="availability-grid">
                     <div>Available:</div>
-                    <div><Switch /></div>
+                    <div>
+                        <Switch checked={available} onChange={this.handleAvailableChange.bind(this)} />
+                    </div>
                     <div>Next Available Date:</div>
                     <div><DatePicker /></div>
                     <div>Minimum Hours:</div>
@@ -28,7 +39,7 @@ export default class AvailabilityEdit extends Component {
                     </div>
                     <div>Hourly Rate:</div>
                     <div><InputNumber /></div>
-                    <div>Notes:</div>
+                    <div style={{ alignSelf: 'start' }}>Notes:</div>
                     <div><Input.TextArea /></div>
                 </div>
             </div>
