e41ad9bc001da908d74a387dae88a27488ee8440
vijayst <vijayst@gmail.com>
2018-07-09 11:55:21 +0530

    availability options

--- src/profile/AvailabilityEdit.js
+++ src/profile/AvailabilityEdit.js
@@ -11,9 +11,21 @@ export default class AvailabilityEdit extends Component {
                     <div>Next Available Date:</div>
                     <div><DatePicker /></div>
                     <div>Minimum Hours:</div>
-                    <div><Select style={{ width: 300 }} /></div>
+                    <div>
+                        <Select style={{ width: 300 }}>
+                            <Select.Option key="10">10 Hours</Select.Option>
+                            <Select.Option key="20">20 Hours</Select.Option>
+                            <Select.Option key="30">30 Hours</Select.Option>
+                            <Select.Option key="40">40 Hours</Select.Option>
+                        </Select>
+                    </div>
                     <div>Preferred Work Type:</div>
-                    <div><Select style={{ width: 300 }} /></div>
+                    <div>
+                        <Select style={{ width: 300 }}>
+                            <Select.Option key="hourly">Hourly</Select.Option>
+                            <Select.Option key="fixed">Fixed Price</Select.Option>
+                        </Select>
+                    </div>
                     <div>Hourly Rate:</div>
                     <div><InputNumber /></div>
                     <div>Notes:</div>
