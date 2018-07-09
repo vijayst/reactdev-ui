7319c4c48d2cece9d985a8d6585d393505904529
Vijay Thirugnanam <vijayst@Vijays-MacBook-Air.local>
2018-07-09 21:16:16 +0530

    work type change

--- src/profile/AvailabilityEdit.js
+++ src/profile/AvailabilityEdit.js
@@ -1,17 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { Input, InputNumber, Select, Switch, DatePicker } from 'antd';
 
 export default class AvailabilityEdit extends Component {
     state = {
-        available: false
+        available: false,
+        workType: 'Hourly'
     };
 
     handleAvailableChange(available) {
         this.setState({ available });
     }
 
+    handleWorkTypeChange(workType) {
+        this.setState({ workType });
+    }
+
     render() {
-        const { available } = this.state;
+        const { available, workType } = this.state;
         return (
             <div className="availability-edit-container">
                 <div className="availability-grid">
@@ -19,26 +24,37 @@ export default class AvailabilityEdit extends Component {
                     <div>
                         <Switch checked={available} onChange={this.handleAvailableChange.bind(this)} />
                     </div>
-                    <div>Next Available Date:</div>
-                    <div><DatePicker /></div>
-                    <div>Minimum Hours:</div>
-                    <div>
-                        <Select style={{ width: 300 }}>
-                            <Select.Option key="10">10 Hours</Select.Option>
-                            <Select.Option key="20">20 Hours</Select.Option>
-                            <Select.Option key="30">30 Hours</Select.Option>
-                            <Select.Option key="40">40 Hours</Select.Option>
-                        </Select>
-                    </div>
+                    {available ? (
+                        <Fragment>
+                            <div>Minimum Hours:</div>
+                            <div>
+                                <Select style={{ width: 300 }}>
+                                    <Select.Option key="10">10 Hours</Select.Option>
+                                    <Select.Option key="20">20 Hours</Select.Option>
+                                    <Select.Option key="30">30 Hours</Select.Option>
+                                    <Select.Option key="40">40 Hours</Select.Option>
+                                </Select>
+                            </div>
+                        </Fragment>
+                    ) : (
+                            <Fragment>
+                                <div>Next Available Date:</div>
+                                <div><DatePicker /></div>
+                            </Fragment>
+                        )}
                     <div>Preferred Work Type:</div>
                     <div>
-                        <Select style={{ width: 300 }}>
-                            <Select.Option key="hourly">Hourly</Select.Option>
-                            <Select.Option key="fixed">Fixed Price</Select.Option>
+                        <Select style={{ width: 300 }} value={workType} onChange={this.handleWorkTypeChange.bind(this)}>
+                            <Select.Option key="Hourly">Hourly</Select.Option>
+                            <Select.Option key="Fixed Price">Fixed Price</Select.Option>
                         </Select>
                     </div>
-                    <div>Hourly Rate:</div>
-                    <div><InputNumber /></div>
+                    {workType === 'Hourly' && (
+                        <Fragment>
+                            <div>Hourly Rate:</div>
+                            <div><InputNumber /></div>
+                        </Fragment>
+                    )}
                     <div style={{ alignSelf: 'start' }}>Notes:</div>
                     <div><Input.TextArea /></div>
                 </div>
