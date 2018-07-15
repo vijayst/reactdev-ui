2555a785f0bb353e247fcf979bd0a961ff1320dd
vijayst <vijayst@gmail.com>
2018-07-15 11:48:33 +0530

    basic validation for input

--- src/profile/PersonalInfoEdit.js
+++ src/profile/PersonalInfoEdit.js
@@ -4,8 +4,18 @@ import { Input, Upload, Icon, DatePicker, Select } from 'antd';
 export default class PersonalInfoEdit extends Component {
     state = {};
 
+    validate() {
+        const { firstName } = this.state;
+        const firstNameError = !firstName ? 'Please enter first name' : '';
+        this.setState({ firstNameError });
+    }
+
+    handleFirstNameChange(e) {
+        this.setState({ firstName: e.target.value });
+    }
+
     render() {
-        const { imageUrl } = this.state;
+        const { imageUrl, firstName, firstNameError } = this.state;
         return (
             <div className="personal-info-edit-container">
                 <div className="photo">
@@ -17,26 +27,55 @@ export default class PersonalInfoEdit extends Component {
                         onPreview={this.handlePreview}
                         onChange={this.handleChange}
                     >
-                        {imageUrl ? <img src={imageUrl} alt="avatar" /> : (
-                        <div>
-                            <Icon type={this.state.loading ? 'loading' : 'plus'} />
-                            <div className="ant-upload-text">Upload</div>
-                        </div>
+                        {imageUrl ? (
+                            <img src={imageUrl} alt="avatar" />
+                        ) : (
+                            <div>
+                                <Icon
+                                    type={
+                                        this.state.loading ? 'loading' : 'plus'
+                                    }
+                                />
+                                <div className="ant-upload-text">Upload</div>
+                            </div>
                         )}
                     </Upload>
                 </div>
                 <div>First name:</div>
-                <div><Input placeholder="First name" /></div>
+                <div>
+                    <Input
+                        placeholder="First name"
+                        value={firstName}
+                        onChange={this.handleFirstNameChange.bind(this)}
+                        className={firstNameError && 'error-control'}
+                    />
+                </div>
+                <div className="error-text">{firstNameError}</div>
                 <div>Last name:</div>
-                <div><Input placeholder="Last name" /></div>
+                <div>
+                    <Input placeholder="Last name" />
+                </div>
+                <div />
                 <div>Email:</div>
-                <div><Input placeholder="Email" /></div>
+                <div>
+                    <Input placeholder="Email" />
+                </div>
+                <div />
                 <div>City:</div>
-                <div><Input placeholder="City" /></div>
+                <div>
+                    <Input placeholder="City" />
+                </div>
+                <div />
                 <div>Country:</div>
-                <div><Select style={{ width: 300 }} /></div>
+                <div>
+                    <Select style={{ width: 300 }} />
+                </div>
+                <div />
                 <div>Date of Birth:</div>
-                <div><DatePicker placeholder="Date of Birth" /></div>
+                <div>
+                    <DatePicker placeholder="Date of Birth" />
+                </div>
+                <div />
             </div>
         );
     }
--- src/profile/ProfileEdit.js
+++ src/profile/ProfileEdit.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import { Tabs, Button } from 'antd';
 import PersonalInfoEdit from './PersonalInfoEdit';
 import ExperienceEdit from './ExperienceEdit';
@@ -6,7 +6,17 @@ import AvailabilityEdit from './AvailabilityEdit';
 import './profileEdit.css';
 
 export default class ProfileEdit extends Component {
-    handleSave() {}
+    constructor() {
+        super();
+        this.personalInfoRef = React.createRef();
+        this.experienceRef = React.createRef();
+        this.availabilityRef = React.createRef();
+    }
+
+    handleSave(e) {
+        e.preventDefault();
+        this.personalInfoRef.current.validate();
+    }
 
     handleCancel() {
         this.props.history.goBack();
@@ -14,25 +24,27 @@ export default class ProfileEdit extends Component {
 
     render() {
         return (
-            <Fragment>
+            <form onSubmit={this.handleSave.bind(this)}>
                 <Tabs defaultActiveKey="1">
                     <Tabs.TabPane tab="Personal Info" key="1">
-                        <PersonalInfoEdit />
+                        <PersonalInfoEdit ref={this.personalInfoRef} />
                     </Tabs.TabPane>
                     <Tabs.TabPane tab="Experience" key="2">
-                        <ExperienceEdit />
+                        <ExperienceEdit ref={this.experienceRef} />
                     </Tabs.TabPane>
                     <Tabs.TabPane tab="Availability" key="3">
-                        <AvailabilityEdit />
+                        <AvailabilityEdit ref={this.availabilityRef} />
                     </Tabs.TabPane>
                 </Tabs>
                 <div className="buttons">
                     <Button onClick={this.handleCancel.bind(this)}>
                         Cancel
                     </Button>
-                    <Button onClick={this.handleSave.bind(this)}>Save</Button>
+                    <Button type="primary" htmlType="submit">
+                        Save
+                    </Button>
                 </div>
-            </Fragment>
+            </form>
         );
     }
 }
