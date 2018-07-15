09896b6b20de07e74bb0bcbed480f17a0f29db7f
Vijay Thirugnanam <vijayst@Vijays-MacBook-Air.local>
2018-07-15 10:34:57 +0530

    profile edit / cancel

--- src/profile/Profile.js
+++ src/profile/Profile.js
@@ -1,24 +1,33 @@
-import React, { Component } from 'react';
-import { Tabs } from 'antd';
+import React, { Component, Fragment } from 'react';
+import { Tabs, Button } from 'antd';
 import PersonalInfo from './PersonalInfo';
 import Experience from './Experience';
 import Availability from './Availability';
 import './profile.css';
 
 export default class Profile extends Component {
+    handleEdit() {
+        this.props.history.push('/profile/edit');
+    }
+
     render() {
         return (
-            <Tabs defaultActiveKey="1">
-                <Tabs.TabPane tab="Personal Info" key="1">
-                    <PersonalInfo />
-                </Tabs.TabPane>
-                <Tabs.TabPane tab="Experience" key="2">
-                    <Experience />
-                </Tabs.TabPane>
-                <Tabs.TabPane tab="Availability" key="3">
-                    <Availability />
-                </Tabs.TabPane>
-            </Tabs>
+            <Fragment>
+                <Tabs defaultActiveKey="1">
+                    <Tabs.TabPane tab="Personal Info" key="1">
+                        <PersonalInfo />
+                    </Tabs.TabPane>
+                    <Tabs.TabPane tab="Experience" key="2">
+                        <Experience />
+                    </Tabs.TabPane>
+                    <Tabs.TabPane tab="Availability" key="3">
+                        <Availability />
+                    </Tabs.TabPane>
+                </Tabs>
+                <div className="buttons">
+                    <Button onClick={this.handleEdit.bind(this)}>Edit</Button>
+                </div>
+            </Fragment>
         );
     }
 }
--- src/profile/ProfileEdit.js
+++ src/profile/ProfileEdit.js
@@ -1,13 +1,18 @@
-import React, { Component } from 'react';
-import { Tabs } from 'antd';
+import React, { Component, Fragment } from 'react';
+import { Tabs, Button } from 'antd';
 import PersonalInfoEdit from './PersonalInfoEdit';
 import ExperienceEdit from './ExperienceEdit';
 import AvailabilityEdit from './AvailabilityEdit';
 import './profileEdit.css';
 
 export default class ProfileEdit extends Component {
+    handleCancel() {
+        this.props.history.goBack();
+    }
+
     render() {
         return (
+            <Fragment>
             <Tabs defaultActiveKey="1">
                 <Tabs.TabPane tab="Personal Info" key="1">
                     <PersonalInfoEdit />
@@ -19,6 +24,11 @@ export default class ProfileEdit extends Component {
                     <AvailabilityEdit />
                 </Tabs.TabPane>
             </Tabs>
+                <div className="buttons">
+                    <Button onClick={this.handleCancel.bind(this)}>Cancel</Button>
+                    <Button>Save</Button>
+                </div>
+            </Fragment>
         );
     }
 }
