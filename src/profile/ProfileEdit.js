3597b95d40ccdf83c944529c6b62b417e6beeafd
vijayst <vijayst@gmail.com>
2018-07-15 11:16:34 +0530

    integrate prettier

--- src/profile/ProfileEdit.js
+++ src/profile/ProfileEdit.js
@@ -6,6 +6,8 @@ import AvailabilityEdit from './AvailabilityEdit';
 import './profileEdit.css';
 
 export default class ProfileEdit extends Component {
+    handleSave() {}
+
     handleCancel() {
         this.props.history.goBack();
     }
@@ -13,20 +15,22 @@ export default class ProfileEdit extends Component {
     render() {
         return (
             <Fragment>
-            <Tabs defaultActiveKey="1">
-                <Tabs.TabPane tab="Personal Info" key="1">
-                    <PersonalInfoEdit />
-                </Tabs.TabPane>
-                <Tabs.TabPane tab="Experience" key="2">
-                    <ExperienceEdit />
-                </Tabs.TabPane>
-                <Tabs.TabPane tab="Availability" key="3">
-                    <AvailabilityEdit />
-                </Tabs.TabPane>
-            </Tabs>
+                <Tabs defaultActiveKey="1">
+                    <Tabs.TabPane tab="Personal Info" key="1">
+                        <PersonalInfoEdit />
+                    </Tabs.TabPane>
+                    <Tabs.TabPane tab="Experience" key="2">
+                        <ExperienceEdit />
+                    </Tabs.TabPane>
+                    <Tabs.TabPane tab="Availability" key="3">
+                        <AvailabilityEdit />
+                    </Tabs.TabPane>
+                </Tabs>
                 <div className="buttons">
-                    <Button onClick={this.handleCancel.bind(this)}>Cancel</Button>
-                    <Button>Save</Button>
+                    <Button onClick={this.handleCancel.bind(this)}>
+                        Cancel
+                    </Button>
+                    <Button onClick={this.handleSave.bind(this)}>Save</Button>
                 </div>
             </Fragment>
         );
