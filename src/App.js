a6cb32f72ac351c2b246533ed1ced59c68a765ff
vijayst <vijayst@gmail.com>
2018-10-01 21:43:51 +0530

    remove auth0

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { ConnectedRouter } from 'connected-react-router';
-import { Switch, Route } from 'react-router-dom';
 import Main from './layout/Main';
-import Callback from './Callback';
 import history from './lib/history';
 import { Provider } from 'react-redux';
 import store from './store/store';
@@ -12,10 +10,7 @@ const App = () => (
     <Provider store={store}>
         <ConnectedRouter history={history}>
             <div className="app">
-                <Switch>
-                    <Route exact path="/callback" component={Callback} />
-                    <Route component={Main} />
-                </Switch>
+                <Main />
             </div>
         </ConnectedRouter>
     </Provider>
