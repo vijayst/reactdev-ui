ac19661e873f4d933d708e3c8a153cc50a9b1f03
vijayst <vijayst@gmail.com>
2018-10-01 09:40:43 +0530

    add password input

--- src/auth/Account.js
+++ src/auth/Account.js
@@ -6,25 +6,39 @@ export default class Account extends Component {
 
     handleNext(e) {
         e.preventDefault();
-        const { email } = this.state;
-        const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        const emailError = regex.test(email)
+        const { email, password, repeat } = this.state;
+        const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        const emailError = emailRegex.test(email)
             ? ''
             : 'Please enter a valid email address';
-        this.setState({ emailError });
-        if (!emailError) {
+        const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+        const passwordError = passwordRegex.test(password)
+            ? ''
+            : 'Password should contain 1 letter, 1 number and should have 8 characters';
+        const repeatError = password !== repeat ? 'Passwords do not match' : '';
+        this.setState({ emailError, passwordError, repeatError });
+        if (!emailError && !passwordError && !repeatError) {
             this.props.history.push('/signup/profile');
         }
     }
 
     render() {
-        const { firstName, lastName, email, emailError } = this.state;
-        const disabled = !firstName || !lastName || !email;
+        const {
+            firstName,
+            lastName,
+            email,
+            password,
+            repeat,
+            emailError,
+            passwordError,
+            repeatError
+        } = this.state;
+        const disabled =
+            !firstName || !lastName || !email || !password || !repeat;
         return (
             <main className="centered">
                 <form className="form">
                     <div className="form__title">Sign Up</div>
-                    <Error message={emailError} />
                     <input
                         type="text"
                         placeholder="First Name"
@@ -45,6 +59,25 @@ export default class Account extends Component {
                         className={emailError && 'error'}
                         onChange={e => this.setState({ email: e.target.value })}
                     />
+                    <Error message={emailError} />
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        className={passwordError && 'error'}
+                        onChange={e =>
+                            this.setState({ password: e.target.value })
+                        }
+                    />
+                    <Error message={passwordError} />
+                    <input
+                        type="password"
+                        placeholder="Repeat Password"
+                        className={repeatError && 'error'}
+                        onChange={e =>
+                            this.setState({ repeat: e.target.value })
+                        }
+                    />
+                    <Error message={repeatError} />
                     <div className="mt24 ralign">
                         <button
                             disabled={disabled}
